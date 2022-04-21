13d2ca8bd4baf8c630d0783e51519ab77d1355da
Evelain <evelain.tengue@gmail.com>
2022-04-21 14:24:22 +0200

    version initiale du projet

---
+++ js/app.js
@@ -0,0 +1,3 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital@1&display=swap" rel="stylesheet"></link>
